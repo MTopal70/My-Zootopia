18cbbd48841f6ed84777f636567c013bfb0d5d11
MTopal70 <muharrem.topal@outlook.com>
2025-09-09 03:02:41 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,7 +8,7 @@ def load_data(file_path):
 # 1. load the file
 animals_data = load_data("animals_data.json")
 
-# 2. generates the animals information as text
+# 2. Generate HTML-Cards
 output = ""
 for animal in animals_data:
     name = animal.get("name")
@@ -16,26 +16,27 @@ for animal in animals_data:
     locations = animal.get("locations", [])
     type_ = animal.get("characteristics", {}).get("type")
 
+    output += '<li class="cards__item">\n'
     if name:
-        output += f"Name: {name}\n"
+        output += f"Name: {name}<br/>\n"
     if diet:
-        output += f"Diet: {diet}\n"
+        output += f"Diet: {diet}<br/>\n"
     if locations:
-        output += f"Location: {locations[0]}\n"
+        output += f"Location: {locations[0]}<br/>\n"
     if type_:
-        output += f"Type: {type_}\n"
-    output += "\n"  # Whitespace between the animals
+        output += f"Type: {type_}<br/>\n"
+    output += '</li>\n'
 
-# 3. load the HTML-Template
+# 3. Load HTML-Template
 with open("animals_template.html", "r", encoding="utf-8") as f:
     template = f.read()
 
-# 4. replace the placeholder
+# 4. replace placeholder
 final_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
 
 # 5. write the new HTML file
 with open("animals.html", "w", encoding="utf-8") as f:
     f.write(final_html)
 
-print("✅ animals.html generated successfully!")
+print("✅ HTML-Cards generated successfully!")
 
